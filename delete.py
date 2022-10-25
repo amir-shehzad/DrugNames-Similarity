45594a83caa7b1c04604ad64354e713b15dc357d
amir-shehzad <amir_shehzad@outlook.com>
2022-10-25 18:13:00 +0500

    remove additional files

--- delete.py
+++
@@ -1,9 +0,0 @@
-import difflib
-
-string1 = "I love to eat apple."
-string2 = "I do not like to eat pineapple."
-
-temp = difflib.SequenceMatcher(None, string1, string2)
-
-print(temp.get_matching_blocks())
-print("Similarity Score: ", temp.ratio())
